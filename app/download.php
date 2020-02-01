3e649fe2c195478ffa1710b054082914d2313bc0
robert <oranje@mail.com>
2020-02-01 01:03:06 +0100

    'wip'

--- app/download.php
+++ app/download.php
@@ -6,24 +6,12 @@ if (isset($_GET['download'])) {
     if (file_exists($filePath)) {
         $mime = mime_content_type($filePath);
         if ($mime) {
-            forceDownload($filePath, $fileName, $mime);
+            viewFile($filePath, $fileName, $mime);
         }
     }
 }
 
-//
-// if you want a pop-up window to show 'open with / save file',
-// then MUST use: header('Content-Disposition: attachment; filename=' . $fileName);
-// Here, $fileName does not have to be the actual path to the file, it is just what is
-// displayed in the pop-up window. We prefer to use the $fileName here, not the full $filePath.
-//
-// 2020/01/31: we turn-off file downloading, we only want to see the files in browser tab.
-
-// 2020/01/31: we find xls files need the 'attachment' header to work correctly. PDF, txt we
-// can view in browser sans probleme. We're going to build an exception code block to turn
-// the 'Content-Disposition: attachment; filename=...' header on when needed. But not for all files, as
-// we prefer them to be opened for viewing in browser directly, not downloaded.
-function forceDownload($filePath = '', $fileName = '', $mime = ''): bool
+function viewFile($filePath = '', $fileName = '', $mime = ''): bool
 {
     $size = filesize($filePath);
     header('Content-Description: File Transfer');
@@ -38,5 +26,9 @@ function forceDownload($filePath = '', $fileName = '', $mime = ''): bool
 
     ob_clean();
     flush();
-    readfile($filePath);
+    if (readfile($filePath)) {
+        return true;
+    } else {
+        return false;
+    }
 }
